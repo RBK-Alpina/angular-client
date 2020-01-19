e85dbd8fa4dd276e7e185958b520e10aa8b914f5
Essam <esamfarg91@gmail.com>
2020-01-19 16:37:58 +0100

    fix bugs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthDetails } from 'src/jwt';
-import { AuthService } from '../services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private authService:AuthService) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authService.isAuthed.subscribe(msg=>{
+    this.authService.isAuthed.subscribe(msg => {
       this.auth = msg;
-    })
+    });
   }
-
-
 
   auth: boolean = false;
 }
